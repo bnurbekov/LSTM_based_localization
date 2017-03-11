bdbab1b40813a5556d211742365d8176521a9336
Batyrlan Nurbekov <batyrlan@bossanova.com>
2017-03-10 17:02:50 -0800

    Adding support for the online mode.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import numpy as np
 import extract_data
 import time
 import math
+import socketserver
+import json
 
 DATA_FILE = "/media/atlancer/My Passport/Projects/summary.mat"
 LOG_DIR = "/media/atlancer/My Passport/Projects/log_dir"
@@ -12,7 +14,7 @@ NUM_STEPS = 30
 BATCH_SIZE = 20
 NUM_HIDDEN = 500
 NUM_LSTM_LAYERS = 2
-INPUT_SIZE = None
+INPUT_SIZE = 1081 # Size of the scan readings array
 OUTPUT_SIZE = 3
 MAX_GRAD_NORM = 5
 INITIALIZER_STD_DEV = 0.1
@@ -24,9 +26,13 @@ LEARNING_RATE = 1
 LEARNING_RATE_DECAY = 0.8
 LEARNING_RATE_DECAY_START_EPOCH_I = 6
 
+ONLINE_MODE = True
+#if ONLINE_MODE is True, then set the following:
+HOST = '192.168.1.65'
+PORT = 7779
 
 class Model:
-	def __init__(self, is_training):
+	def __init__(self, batch_size, num_steps, is_training=False):
 		self.lstm = tf.contrib.rnn.BasicLSTMCell(NUM_HIDDEN, forget_bias=0.0, state_is_tuple=True)
 
 		if is_training and KEEP_PROB < 1:
@@ -34,10 +40,10 @@ class Model:
 
 		self.stacked_lstm = tf.contrib.rnn.MultiRNNCell([self.lstm]*NUM_LSTM_LAYERS, state_is_tuple=True)
 
-		self.initial_state = self.stacked_lstm.zero_state(BATCH_SIZE, dtype=tf.float32)
+		self.initial_state = self.stacked_lstm.zero_state(batch_size, dtype=tf.float32)
 
-		self.epoch_data = tf.placeholder(tf.float32, shape=[NUM_STEPS, BATCH_SIZE, INPUT_SIZE])
-		self.targets = tf.placeholder(tf.float32, shape=[NUM_STEPS, BATCH_SIZE, OUTPUT_SIZE])
+		self.epoch_data = tf.placeholder(tf.float32, shape=[num_steps, batch_size, INPUT_SIZE])
+		self.targets = tf.placeholder(tf.float32, shape=[num_steps, batch_size, OUTPUT_SIZE])
 
 		if is_training and KEEP_PROB < 1:
 			self.targets = tf.nn.dropout(self.targets, keep_prob=KEEP_PROB)
@@ -48,7 +54,7 @@ class Model:
 		out_weight = tf.get_variable("out_weight", [self.stacked_lstm.output_size, OUTPUT_SIZE], dtype=tf.float32)
 		out_bias = tf.get_variable("out_bias", [OUTPUT_SIZE], dtype=tf.float32)
 		with tf.variable_scope("RNN"):
-			for step_i in range(NUM_STEPS):
+			for step_i in range(num_steps):
 				if step_i > 0:
 					tf.get_variable_scope().reuse_variables()
 				output, state = self.stacked_lstm(self.epoch_data[step_i, :, :], state)
@@ -56,6 +62,8 @@ class Model:
 				out_coordinates = tf.matmul(output, out_weight) + out_bias
 
 				outputs.append(out_coordinates)
+
+		self.outputs = outputs
 
 		self.difference = tf.subtract(self.targets, outputs)
 		self.loss = tf.reduce_mean(tf.reduce_mean(tf.reduce_sum(tf.square(self.difference), axis=2), axis=1))
@@ -77,73 +85,116 @@ class Model:
 	def assign_learning_rate(self, sess, new_value):
 		sess.run(self.learning_rate_update, feed_dict={self.new_learning_rate:new_value})
 
-def run(m, sess, data, targets, is_training, verbose=False):
-	start_time = time.time()
-	total_loss = 0.0
-	iters = 0
+	def run(self, sess, data, targets, is_training, verbose=False, record_outputs=False):
+		start_time = time.time()
+		total_loss = 0.0
+		iters = 0
 
-	fetches = {"final_state":m.final_state, "loss":m.loss}
-	if is_training:
-		fetches["train_op"] = m.train_op
-	else:
-		fetches["diff"] = m.difference
-
-	state = sess.run(m.initial_state)
-	epoch_size = data.shape[0]
-
-	for epoch_i in range(epoch_size):
+		fetches = {"final_state":self.final_state, "loss":self.loss}
 		if is_training:
-			learning_rate_decay = LEARNING_RATE_DECAY ** max(epoch_i - LEARNING_RATE_DECAY_START_EPOCH_I, 0)
-			m.assign_learning_rate(sess, LEARNING_RATE * learning_rate_decay)
+			fetches["train_op"] = self.train_op
+		else:
+			fetches["diff"] = self.difference
+			if record_outputs:
+				fetches["outputs"] = self.outputs
 
-		feed_dict = {m.initial_state: state, m.epoch_data: data[epoch_i], m.targets: targets[epoch_i]}
+		state = sess.run(self.initial_state)
+		epoch_size = data.shape[0]
+		epoch_outputs = []
 
-		vals = sess.run(fetches, feed_dict)
-		state = vals["final_state"]
-		total_loss += vals["loss"]
-		iters += NUM_STEPS
+		for epoch_i in range(epoch_size):
+			if is_training:
+				learning_rate_decay = LEARNING_RATE_DECAY ** max(epoch_i - LEARNING_RATE_DECAY_START_EPOCH_I, 0)
+				self.assign_learning_rate(sess, LEARNING_RATE * learning_rate_decay)
 
-		if verbose:
-			print("Iteration: %d, Speed: %.3f, Loss: %.3f" % (iters, iters*BATCH_SIZE/(time.time()-start_time), vals["loss"]))
+			feed_dict = {self.initial_state: state, self.epoch_data: data[epoch_i]}
 
-		if not is_training and vals["loss"] > 0.005:
-			print("Something is wrong")
+			if targets is not None and not is_training:
+				feed_dict[self.targets] = targets
 
-	return total_loss/epoch_size
+			vals = sess.run(fetches, feed_dict)
+			state = vals["final_state"]
+			total_loss += vals["loss"]
+			iters += NUM_STEPS
+
+			if verbose:
+				if not is_training and targets is not None:
+					av_perc =np.mean(np.mean(np.mean(np.fabs(np.divide(vals["diff"], targets[epoch_i])), axis=2), axis=1))
+				else:
+					av_perc = -1
+				print("Iteration: %d, Speed: %.3f, Loss: %.3f, Av Perc: %f" % (iters, iters*BATCH_SIZE/(time.time()-start_time), vals["loss"], av_perc))
+
+			if not is_training and vals["loss"] > 0.005:
+				print("Something is wrong. Loss: ", vals["loss"])
+
+			if record_outputs:
+				epoch_outputs.append(vals["outputs"])
+
+		return epoch_outputs
+
+def MakeHandlerClassFromArgv(model, session):
+	class MyTCPHandler(socketserver.StreamRequestHandler, object):
+		def __init__(self, *args, **kwargs):
+			self.model = model
+			self.session = session
+			super(MyTCPHandler, self).__init__(*args, **kwargs)
+
+		"""
+		The request handler class for our server.
+
+		It is instantiated once per connection to the server, and must
+		override the handle() method to implement communication to the
+		client.
+		"""
+
+		def handle(self):
+			# self.request is the TCP socket connected to the client
+			while True:
+				self.data = self.rfile.readline().strip()
+				if not self.data:
+					break
+				print("{} wrote:".format(self.client_address[0]))
+				print(self.data)
+
+				arrData = np.array(json.loads(self.data.decode("utf-8")))
+
+				model_data = np.expand_dims(np.expand_dims(np.expand_dims(arrData, axis=0), axis=0), axis=0)
+
+				epoch_data = self.model.run(session, model_data, np.array([[[0, 0, 0]]]), False, verbose=True, record_outputs=True)
+				# just send back the same data, but upper-cased
+				self.request.sendall(bytes(json.dumps(epoch_data[0][0][0].tolist()), "utf-8"))
+
+	return MyTCPHandler
 
 def main(_):
-	data, targets = extract_data.get_data_from_file(DATA_FILE)
+	if not ONLINE_MODE:
+		data, targets = extract_data.get_data_from_file(DATA_FILE)
 
-	print("Data shape:", data.shape)
-	print("Targets shape:", targets.shape)
+		print("Data shape:", data.shape)
+		print("Targets shape:", targets.shape)
 
-	prev = np.copy(targets[0])
-	for i in range(targets.shape[0]):
-		current = np.subtract(targets[i], prev)
-		current[2] = math.atan2(math.sin(current[2]), math.cos(current[2]))
-		prev = np.copy(targets[i])
-		targets[i] = current
+		prev = np.copy(targets[0])
+		for i in range(targets.shape[0]):
+			current = np.subtract(targets[i], prev)
+			current[2] = math.atan2(math.sin(current[2]), math.cos(current[2]))
+			prev = np.copy(targets[i])
+			targets[i] = current
 
-	print("New Data")
+		print("New Data")
 
-	data, targets = extract_data.slice_data(data, targets, BATCH_SIZE, NUM_STEPS)
+		data, targets = extract_data.slice_data(data, targets, BATCH_SIZE, NUM_STEPS)
+		data = np.divide(data, data.max())
 
-	data = np.divide(data, data.max())
+		print("Cut data shape:", data.shape)
+		print("Cut targets shape:", targets.shape)
 
-	print("Cut data shape:", data.shape)
-	print("Cut targets shape:", targets.shape)
+		data, targets = np.transpose(data, [0, 2, 1, 3]), np.transpose(targets, [0, 2, 1, 3])
 
-	data, targets = np.transpose(data, [0, 2, 1, 3]), np.transpose(targets, [0, 2, 1, 3])
+		print("Transposed data shape:", data.shape)
+		print("Transposed targets shape:", targets.shape)
 
-	print("Transposed data shape:", data.shape)
-	print("Transposed targets shape:", targets.shape)
-
-	training_data, training_targets = data[data.shape[0]//10:, :, :, :], targets[targets.shape[0]//10:, :, :, :]
-	valid_data, valid_targets = data[:data.shape[0]//10, :, :, :], targets[:targets.shape[0]//10, :, :, :]
-
-	global INPUT_SIZE
-
-	INPUT_SIZE = data.shape[-1]
+		training_data, training_targets = data[data.shape[0]//100:, :, :, :], targets[targets.shape[0]//100:, :, :, :]
+		valid_data, valid_targets = data[:data.shape[0]//100, :, :, :], targets[:targets.shape[0]//100, :, :, :]
 
 	with tf.Graph().as_default():
 		# Build model
@@ -152,20 +203,29 @@ def main(_):
 
 		with tf.name_scope("Train"):
 			with tf.variable_scope("Model", reuse=None, initializer=initializer):
-				train_model = Model(True)
+				train_model = Model(BATCH_SIZE, NUM_STEPS, is_training=True)
 		print("Done initializing training model!")
 
 		with tf.name_scope("Valid"):
 			with tf.variable_scope("Model", reuse=True, initializer=initializer):
-				valid_model = Model(False)
+				valid_model = Model(BATCH_SIZE, NUM_STEPS, is_training=False)
 		print("Done initializing valid model!")
+
+		with tf.name_scope("Online"):
+			with tf.variable_scope("Model", reuse=True, initializer=initializer):
+				online_model = Model(1, 1, is_training=False)
+		print("Done initializing online model!")
 
 		sv = tf.train.Supervisor(logdir=LOG_DIR)
 		with sv.managed_session() as sess:
-			av_training_loss = run(train_model, sess, training_data, training_targets, True, verbose=True)
-			print("Training loss: %f", av_training_loss)
-			av_valid_loss = run(valid_model, sess, valid_data, valid_targets, False, verbose=True)
-			print("Valid loss: %f", av_valid_loss)
+			if not ONLINE_MODE:
+				train_model.run(sess, training_data, training_targets, True, verbose=True)
+				valid_model.run(sess, valid_data, valid_targets, False, verbose=True)
+			else:
+				socketserver.TCPServer.allow_reuse_address = True
+				server = socketserver.TCPServer((HOST, PORT), MakeHandlerClassFromArgv(online_model, sess))
+				print("Running server!")
+				server.serve_forever()
 
 			sv.saver.save(sess, LOG_DIR, global_step=sv.global_step)
 
